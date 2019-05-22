fb6e50f3eeff5a3df4f90e6c75806bc758366ca2
jaleenasimons <34337890+jaleenasimons@users.noreply.github.com>
2019-05-22 12:49:14 +0200

    webtypografie eind

---
+++ web-typography-18-19/closed-captions/captions.js
@@ -0,0 +1,55 @@
+// Teddy?
+captions[0] = ['7.56','8.9'];
+captions[1] = ['12.52','13.76'];
+captions[2] = ['18.2','19.56'];
+// Hug me!
+captions[3] = ['27.5','28.62'];
+captions[4] = ['29.0','29.96'];
+captions[5] = ['30.1','32.3'];
+// Did you.. Did you just talk?
+captions[6] = ['32.5','35.14'];
+captions[7] = ['35.2','39.1'];
+captions[8] = ['39.7','42.84'];
+// Well here I am
+captions[9] = ['43.14','45.16'];
+captions[10] = ['45.12','48.06'];
+captions[11] = ['48.02','49.34'];
+// Forever and ever?
+captions[12] = ['49.5','51.14'];
+captions[13] = ['51.18','52.5'];
+captions[14] = ['52.92','53.82'];
+// John was just about the happiest boy in the world.
+captions[15] = ['54.22','57.46'];
+captions[16] = ['57.46','61'];
+captions[17] = ['61.4','64.2'];
+// Really? Well isn't that exciting?
+captions[18] = ['64.679','67.24'];
+captions[19] = ['67.34','69.54'];
+captions[20] = ['70.3','72.36'];
+// Screams
+captions[21] = ['72.36','73.96'];
+captions[22] = ['73.8','75.22'];
+captions[23] = ['75.2','76.95'];
+// Oh my God, oh God!
+captions[24] = ['77','78.08'];
+captions[25] = ['77.88','80.08'];
+captions[26] = ['80.04','80.86'];
+// GET OVER HERE!
+captions[27] = ['80.66','81.68'];
+captions[28] = ['81.94','83.14'];
+captions[29] = ['83.26','84.3'];
+// Dad no!
+captions[30] = ['84.3','85.24'];
+captions[31] = ['85.26','86.6'];
+captions[32] = ['86.5','88'];
+// Helen cries
+captions[33] = ['87.88','88.7'];
+captions[34] = ['88.58','91.4'];
+captions[35] = ['91.42','93.62'];
+// Yeah dad! I made a wish last night that teddy was alive, and my wish came true!
+captions[36] = ['93.66','98.56'];
+captions[37] = ['100.38','102.12'];
+captions[38] = ['103.6','105.4'];
+// It's a christmas miracle
+captions[39] = ['106.08','108.28'];
+captions[40] = ['109.16','112'];
---
+++ web-typography-18-19/closed-captions/sounds.js
@@ -0,0 +1,13 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+0,
+28.7,
+32,
+54,
+72.5, 
+73.7,
+80.5,
+82,
+88.5
+];
